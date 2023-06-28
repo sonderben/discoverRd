030409f27aef3352aee9dcdcc832a09288b66184
sonderben <sonderben@gmail.com>
2023-06-28 16:08:25 -0400

    Initial Commit

---
+++ discoverRd/discoverRdApp.swift
@@ -0,0 +1,17 @@
+//
+//  discoverRdApp.swift
+//  discoverRd
+//
+//  Created by Benderson Phanor on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct discoverRdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
